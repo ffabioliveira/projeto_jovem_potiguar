24b3c9bf97cc88d3d8240a848756345dae636d56
ffabioliveira <ffabioliveira81@gmail.com>
2024-12-09 20:48:08 -0300

    primeiro programa

---
+++ atividade01.js
@@ -0,0 +1,8 @@
+//Declare uma variável para armazenar o nome de um aluno, sua idade, e 
+//sua nota final. Exiba valores no console.
+
+let nome = "Fábio Oliveira"
+let idade = 18
+let notaFinal = 9.5
+
+console.log("Nome: ", nome, "Idade: ", idade, "Nota Final: ", notaFinal)
